90a00fb9ee72a9a3c13a098c7c56cd7c7edc05ba
Stephan Kleine <stephan.kleine@gmail.com>
2013-04-16 01:37:51 +0200

    Properly validate URL and code cleanup

--- source/src/main/java/com/atlassian/stash/plugins/jenkins/JenkinsBuildTrigger.java
+++ source/src/main/java/com/atlassian/stash/plugins/jenkins/JenkinsBuildTrigger.java
@@ -4,11 +4,16 @@ import com.atlassian.stash.hook.repository.*;
 import com.atlassian.stash.repository.*;
 import com.atlassian.stash.server.ApplicationPropertiesService;
 import com.atlassian.stash.setting.*;
+import org.apache.commons.lang.StringUtils;
+
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collection;
 
 public class JenkinsBuildTrigger implements AsyncPostReceiveRepositoryHook, RepositorySettingsValidator
 {
+    private static final String PROPERTY_URL = "url";
+
     private final ApplicationPropertiesService applicationProperties;
 
     public JenkinsBuildTrigger(ApplicationPropertiesService applicationProperties) {
@@ -24,17 +29,14 @@ public class JenkinsBuildTrigger implements AsyncPostReceiveRepositoryHook, Repo
         String projectKey = context.getRepository().getProject().getKey();
         String repositoryName = context.getRepository().getName();
 
-        String url = context.getSettings().getString("url") + "/git/notifyCommit?url=http://" +
+        String url = context.getSettings().getString(PROPERTY_URL) + "/git/notifyCommit?url=http://" +
                 applicationProperties.getBaseUrl().getHost() + "/" + projectKey + "/" + repositoryName + ".git";
 
-        if (url != null)
-        {
-            try
-            {
+        if (url != null) {
+            try {
                 new URL(url).openConnection().getInputStream().close();
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -43,9 +45,15 @@ public class JenkinsBuildTrigger implements AsyncPostReceiveRepositoryHook, Repo
     @Override
     public void validate(Settings settings, SettingsValidationErrors errors, Repository repository)
     {
-        if (settings.getString("url", "").isEmpty())
-        {
-            errors.addFieldError("url", "Url field is blank, please supply one");
+        String url = settings.getString(PROPERTY_URL, "");
+        if (StringUtils.isEmpty(url)) {
+            errors.addFieldError(PROPERTY_URL, "Url field is blank, please supply one");
+        } else {
+            try {
+                new URL(url);
+            } catch (MalformedURLException e) {
+                errors.addFieldError(PROPERTY_URL, "Please supply a valid URL");
+            }
         }
     }
 }
