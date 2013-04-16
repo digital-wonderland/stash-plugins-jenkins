a8922bca4ee9064b66774882f883fab7b78cda4c
Stephan Kleine <stephan.kleine@gmail.com>
2013-04-16 21:50:06 +0200

    add admin interface

---
+++ source/src/main/java/com/atlassian/stash/plugins/jenkins/ConfigResource.java
@@ -0,0 +1,91 @@
+package com.atlassian.stash.plugins.jenkins;
+
+import com.atlassian.sal.api.pluginsettings.PluginSettings;
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
+import com.atlassian.sal.api.transaction.TransactionCallback;
+import com.atlassian.sal.api.transaction.TransactionTemplate;
+import com.atlassian.sal.api.user.UserManager;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stephan
+ * Date: 16.4.2013
+ * Time: 19:13
+ * To change this template use File | Settings | File Templates.
+ */
+@Path("/")
+public class ConfigResource {
+
+    private final static String POSTFIX_URL = ".url";
+
+    private final UserManager userManager;
+    private final PluginSettingsFactory pluginSettingsFactory;
+    private final TransactionTemplate transactionTemplate;
+
+    public ConfigResource(final UserManager userManager, final PluginSettingsFactory pluginSettingsFactory, final TransactionTemplate transactionTemplate) {
+        this.userManager = userManager;
+        this.pluginSettingsFactory = pluginSettingsFactory;
+        this.transactionTemplate = transactionTemplate;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(@Context final HttpServletRequest request) {
+        final String username = userManager.getRemoteUsername(request);
+        if (username == null || !userManager.isSystemAdmin(username)) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+        return Response.ok(transactionTemplate.execute(new TransactionCallback() {
+            public Object doInTransaction() {
+                PluginSettings settings = pluginSettingsFactory.createGlobalSettings();
+                Config config = new Config();
+                config.setUrl((String) settings.get(Config.class.getName() + POSTFIX_URL));
+                return config;
+            }
+        })).build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response put(final Config config, @Context final HttpServletRequest request) {
+        String username = userManager.getRemoteUsername(request);
+        if (username == null || !userManager.isSystemAdmin(username)) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+        transactionTemplate.execute(new TransactionCallback() {
+            public Object doInTransaction() {
+                PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
+                pluginSettings.put(Config.class.getName() + POSTFIX_URL, config.getUrl());
+                return null;
+            }
+        });
+        return Response.noContent().build();
+    }
+
+    @XmlRootElement
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static final class Config
+    {
+        @XmlElement private String url;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(final String url) {
+            this.url = url;
+        }
+    }
+}
---
+++ source/src/main/resources/js/admin.js
@@ -0,0 +1,46 @@
+AJS.toInit(function() {
+    var baseUrl = AJS.$("meta[name='application-base-url']").attr("content");
+
+    function populateForm() {
+        AJS.$.ajax({
+            url: baseUrl + "/rest/jenkins/1.0/",
+            dataType: "json",
+            success: function(config) {
+                AJS.$("#url").attr("value", config.url);
+            }
+        });
+    }
+
+    function updateConfig() {
+        AJS.$.ajax({
+            url: baseUrl + "/rest/jenkins/1.0/",
+            type: "PUT",
+            contentType: "application/json",
+            data: '{ "url": "' + AJS.$("#url").attr("value") + '" }',
+            processData: false
+        });
+    }
+
+    populateForm();
+
+    AJS.$("#admin").submit(function(e) {
+        e.preventDefault();
+        updateConfig();
+    });
+
+    AJS.$(function(){
+        AJS.$("#content-2").hide();
+        AJS.$("#general-nav-item").click(function(e){
+            navigateTo(e.target, "content-1");
+        });
+        AJS.$("#advanced-nav-item").click(function(e){
+            navigateTo(e.target, "content-2");
+        });
+        function navigateTo(trigger, contentId){
+            AJS.$("#main-nav li").removeClass("aui-nav-selected");
+            AJS.$(trigger).parent().addClass("aui-nav-selected");
+            AJS.$(".nav-content").hide();
+            AJS.$("#" + contentId).show();
+        }
+    })
+});
